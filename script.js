31d53b22aff018ec3f2a6c2bb218e0fa3f19bb76
Arnav Kumar <32623368+arnavcs@users.noreply.github.com>
2021-03-29 12:13:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
 window.onload = () => {
-    let buttons = document.getElementsByTagName("h2");
     
-    function expand(event) {
-        if (this.nextElementSibling.style.display == "none") {
-            for (let button of buttons) {
-                button.nextElementSibling.style.display = "none";
-            }
-            this.nextElementSibling.style.display = "flex";
-        } else {
-            this.nextElementSibling.style.display = "none";
-        }
-    }
-
-    for (let button of buttons) {
-        button.nextElementSibling.style.display = "none";
-        button.addEventListener('click', expand);
-    }
 }
