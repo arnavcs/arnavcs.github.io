52074e31e214f2510f0e9b2011c0b04c190c804b
Arnav Kumar <arnav.kumar.sri@gmail.com>
2024-02-02 18:39:17 -0500

    new info

--- competition/build.sh
+++ competition/build.sh
@@ -2,4 +2,8 @@
 #
 # build.sh
 
-pandoc --standalone --mathml --toc --template template.html --number-sections main.md -o ./reference.html
+pandoc --standalone\
+       --mathml\
+       --template template.html\
+       --number-sections\
+       main.md -o ./reference.html
