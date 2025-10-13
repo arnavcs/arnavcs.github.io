6738d021c662319d9a6c8bd8c0a4a49388871e1a
Arnav Kumar <arnav.kumar.sri@gmail.com>
2025-10-13 11:32:54 -0600

    resume work

--- resume-cv/build.sh
+++ resume-cv/build.sh
@@ -2,7 +2,10 @@
 #
 # ./build.sh
 
-pdflatex -jobname=resume "\newcommand{\version}{resume}\input{./arnav-kumar.tex}"
-pdflatex -jobname=cv "\newcommand{\version}{cv}\input{./arnav-kumar.tex}"
+pdflatex -jobname=cv "\newcommand{\version}{cv}\newcommand{\focus}{general}\input{./arnav-kumar.tex}"
+pdflatex -jobname=resume "\newcommand{\version}{resume}\newcommand{\focus}{general}\input{./arnav-kumar.tex}"
+pdflatex -jobname=wwresume "\newcommand{\version}{resume}\newcommand{\focus}{general}\newcommand{\board}{waterlooworks}\input{./arnav-kumar.tex}"
+pdflatex -jobname=graphics "\newcommand{\version}{graphics}\newcommand{\focus}{graphics}\input{./arnav-kumar.tex}"
+pdflatex -jobname=wwgraphics "\newcommand{\version}{graphics}\newcommand{\focus}{graphics}\newcommand{\board}{waterlooworks}\input{./arnav-kumar.tex}"
 
 rm ./*.aux ./*.log ./*.out
