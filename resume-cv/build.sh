af20f65d5a8f794cac5ee1f6ff36b6dd88207d31
Arnav Kumar <arnav.kumar.sri@gmail.com>
2025-11-24 06:06:07 -0700

    switched to new latex macros and added math resume

--- resume-cv/build.sh
+++ resume-cv/build.sh
@@ -3,9 +3,11 @@
 # ./build.sh
 
 pdflatex -jobname=cv "\newcommand{\version}{cv}\newcommand{\focus}{general}\input{./arnav-kumar.tex}"
-pdflatex -jobname=resume "\newcommand{\version}{general}\input{./arnav-kumar.tex}"
-pdflatex -jobname=wwresume "\newcommand{\version}{general}\newcommand{\board}{waterlooworks}\input{./arnav-kumar.tex}"
+pdflatex -jobname=resume "\input{./arnav-kumar.tex}"
+pdflatex -jobname=wwresume "\newcommand{\board}{waterlooworks}\input{./arnav-kumar.tex}"
 pdflatex -jobname=graphics "\newcommand{\version}{graphics}\input{./arnav-kumar.tex}"
 pdflatex -jobname=wwgraphics "\newcommand{\version}{graphics}\newcommand{\board}{waterlooworks}\input{./arnav-kumar.tex}"
+pdflatex -jobname=math "\newcommand{\version}{math}\input{./arnav-kumar.tex}"
+pdflatex -jobname=wwmath "\newcommand{\version}{math}\newcommand{\board}{waterlooworks}\input{./arnav-kumar.tex}"
 
 rm ./*.aux ./*.log ./*.out
