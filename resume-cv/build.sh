fde7905105f8fcfae3c6e40ff69268a328c18103
Arnav Kumar <arnav.kumar.sri@gmail.com>
2025-11-24 05:14:24 -0700

    switched latex resume building system

--- resume-cv/build.sh
+++ resume-cv/build.sh
@@ -3,9 +3,9 @@
 # ./build.sh
 
 pdflatex -jobname=cv "\newcommand{\version}{cv}\newcommand{\focus}{general}\input{./arnav-kumar.tex}"
-pdflatex -jobname=resume "\newcommand{\version}{resume}\newcommand{\focus}{general}\input{./arnav-kumar.tex}"
-pdflatex -jobname=wwresume "\newcommand{\version}{resume}\newcommand{\focus}{general}\newcommand{\board}{waterlooworks}\input{./arnav-kumar.tex}"
-pdflatex -jobname=graphics "\newcommand{\version}{graphics}\newcommand{\focus}{graphics}\input{./arnav-kumar.tex}"
-pdflatex -jobname=wwgraphics "\newcommand{\version}{graphics}\newcommand{\focus}{graphics}\newcommand{\board}{waterlooworks}\input{./arnav-kumar.tex}"
+pdflatex -jobname=resume "\newcommand{\version}{general}\input{./arnav-kumar.tex}"
+pdflatex -jobname=wwresume "\newcommand{\version}{general}\newcommand{\board}{waterlooworks}\input{./arnav-kumar.tex}"
+pdflatex -jobname=graphics "\newcommand{\version}{graphics}\input{./arnav-kumar.tex}"
+pdflatex -jobname=wwgraphics "\newcommand{\version}{graphics}\newcommand{\board}{waterlooworks}\input{./arnav-kumar.tex}"
 
 rm ./*.aux ./*.log ./*.out
